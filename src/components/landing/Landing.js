2a9bfc77ef3c18af7c055fedd2dffee6addffc1f
Tyler Corbett <thetylercorbett@gmail.com>
2019-12-07 18:37:07 -0800

    first commit

---
+++ src/components/landing/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Landing.scss';
+
+const Landing = () => {
+  return (
+    <section className="Landing">
+      <h1 className="slogan">Synergize Your Electrolytes</h1>
+    </section>
+  );
+};
+
+export default Landing;
