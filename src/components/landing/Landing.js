d91079017f513136dcc140a8d9b571ae99343c6e
Tyler Corbett <thetylercorbett@gmail.com>
2019-12-07 19:29:18 -0800

    cta button added

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -4,11 +4,11 @@ import './Landing.scss';
 const Landing = () => {
   return (
     <section className="Landing">
-      <h1 className="slogan">Synergize Your Electrolytes</h1>
+      <h1 className="slogan">Electricians you can trust</h1>
       <div className="video-container">
         <video playsinline muted loop autoPlay src='https://res.cloudinary.com/dmtn5fbdu/video/upload/v1575773293/Hogan%20Electric/compressed.mp4'/>
       </div>
-      
+      <button className="cta" onClick={() => alert('request a quote clicked')}>Request a quote</button>
     </section>
   );
 };
