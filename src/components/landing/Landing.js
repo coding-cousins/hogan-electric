a03961a5e24f7533e727affa5364a6b19cdd4db4
Tyler Corbett <thetylercorbett@gmail.com>
2019-12-07 20:19:24 -0800

    fixes non repsonsive header

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -7,8 +7,10 @@ const Landing = () => {
       <div className="video-container">
         <video playsinline muted loop autoPlay src='https://res.cloudinary.com/dmtn5fbdu/video/upload/v1575773293/Hogan%20Electric/compressed.mp4'/>
       </div>
-      <h1 className="slogan">Electricians <span>you</span> can trust</h1>
-      <button className="cta" onClick={() => alert('request a quote clicked')}>Request a quote</button>
+      <div className="slogan-cta">
+        <h1 className="slogan">Electricians <span>you</span> can trust</h1>
+        <button className="cta" onClick={() => alert('request a quote clicked')}>Request a quote</button>
+      </div>
     </section>
   );
 };
