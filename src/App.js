714baf2212fc6fb72874d77ed5521f094d3d1563
Tyler Corbett <thetylercorbett@gmail.com>
2019-12-07 20:15:29 -0800

    about onscreen

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Landing from './components/landing/Landing';
 import Header from './components/header/Header';
+import About from './components/about/About';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Landing/>
+      <About/>
     </div>
   );
 }
---
+++ src/components/about/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.scss';
+
+const About = () => {
+  return (
+    <section className="About">
+      <p>My name is Tyler and I'm typing on a keyboard. type type type its all i do.</p>
+    </section>
+  );
+};
+
+export default About;
