c4c1f827064dcb57ca03ac1638e0e2cbdd3600e1
Tyler Corbett <thetylercorbett@gmail.com>
2019-12-07 20:04:57 -0800

    landing changes

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -4,10 +4,10 @@ import './Landing.scss';
 const Landing = () => {
   return (
     <section className="Landing">
-      <h1 className="slogan">Electricians you can trust</h1>
       <div className="video-container">
         <video playsinline muted loop autoPlay src='https://res.cloudinary.com/dmtn5fbdu/video/upload/v1575773293/Hogan%20Electric/compressed.mp4'/>
       </div>
+      <h1 className="slogan">Electricians <span>you</span> can trust</h1>
       <button className="cta" onClick={() => alert('request a quote clicked')}>Request a quote</button>
     </section>
   );
